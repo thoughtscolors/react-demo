cfd6a33deddb97f9ebcd86ea4da7568591aa90e7
Brady <vanderbannond@gmail.com>
2018-04-12 14:09:09 -0700

    shopping cart demo working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import CartHeader from './components/CartHeader'
 import CartFooter from './components/CartFooter'
 import CartItems from './components/CartItems'
-
+//
 
 class App extends Component {
   render() {
